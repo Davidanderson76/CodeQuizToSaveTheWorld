0dcda76a56f58d29206610631f869985fdfa54b2
David Anderson <david.anderson1993@gmail.com>
2020-09-29 19:24:21 -0500

    adding to the assets file and rearranging the script for better flow

--- script.js
+++ script.js
@@ -23,90 +23,100 @@ const scoreContainer = document.getElementById("scoreContainer");
 let questions = [
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/spiderman.jpg",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/thor.jpg",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/hulk.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/blackpanther.jpg",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/blackwidow.jpg",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/scarletwitch.jpg",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/milesmorales.jpg",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/captainamerica.jpg",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/wintersoldier.jpg",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
     },
     {
         question : "Who is THIS superhero?",
-        imgSrc : "",
-        choiceA : "",
-        choiceB : "",
-        choiceC : "",
-        correct : ""
+        imgSrc : "Assets/stanlee.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
     },
 ];
 
 // Variables
 
-let lastQuestionIndex = questions.length - 1;
+
+const lastQuestionIndex = questions.length - 1;
 let runningQuestionIndex = 0;
+let timer;
+let score = 0; 
+
+//Variables for making the timer work on each individual question
+
+const questionTime = 10;
+const timerWidth = 150;
+let count = 0;
+const timerProgress = timerWidth/questionTime;
 
 //function to make questions based off of info in array
 
@@ -119,6 +129,19 @@ function renderQuestion(){
     choiceC.innerHTML = q.choiceC;
 }
 
+//START QUIZ BUTTON
+
+start.addEventListener("click", startQuiz);
+
+function startQuiz(){
+    start.style.display = "none";
+    counterRender();
+    timer = setInterval(counterRender,1000);
+    progress();
+    renderQuestion();
+    quiz.style.display = "block";
+}
+
 //showing progress of correct and incorrect answers
 
 function progress(){
@@ -126,23 +149,6 @@ function progress(){
         progress.innerHTML += "<div class = 'prog' id = " + qIndex + "> </div>";
     }
 }
-
-//Shows Correct Answer
-function answerCorrect(){
-    document.getElementById(runningQuestionIndex).style.backgroundColor = "green"
-}
-
-//Shows Wrong Answer
-function answerWrong(){
-    document.getElementById(runningQuestionIndex).style.backgroundColor = "red";
-}
-
-//Making the timer work on each individual question
-
-const questionTime = 10;
-const timerWidth = 150;
-let count = 0;
-const timerProgress = timerWidth/questionTime;
 
 //Making the timer show the status of how much time you have left
 
@@ -163,4 +169,50 @@ function counterRender(){
         } 
     }
 }
+
+// Checking the Answers to be correct
+
+function checkAns(answer){
+    if(questions[runningQuestionIndex].correct == answer){
+        score++;
+        answerCorrect();
+    }else{
+        answerWrong();
+    }
+    if(runningQuestionIndex < lastQuestionIndex){
+        count = 0;
+        runningQuestionIndex++;
+        renderQuestion();
+    }else{
+        clearInterval(timer);
+        scoreRender();
+    }
+}
+
+
+
+//Shows Correct Answer
+function answerCorrect(){
+    document.getElementById(runningQuestionIndex).style.backgroundColor = "green"
+}
+
+//Shows Wrong Answer
+function answerWrong(){
+    document.getElementById(runningQuestionIndex).style.backgroundColor = "red";
+}
+
+
+//giving a score function
+
+function score(){
+    scoreContainer.style.display = "block";
+    let scorePercent = Math.round(100 * score / questions.length);
+    let img =   ( scorePercent >= 90) ? "Assets/1GOOD.png" :
+                ( scorePercent >= 80) ? "Assets/2OK.png" :
+                ( scorePercent >= 70) ? "Assets/3MEH.png" :
+                ( scorePercent >= 60) ? "Assets/4EHH.png" :
+                ( scorePercent <= 50) ? "Assets/5BAD.png" :
+    scoreContainer.innerHTML = "<img src =" + img + "> <p>" + scorePercent + "%</p>";
+}
+
 
